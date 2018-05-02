51190e8c781eb2816ec5d33900d73ca87d80219c
gitbulk <giacatiensinh@hotmail.com>
2018-05-02 15:58:49 +0700

    [f] how to use scope

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,3 +1,6 @@
 class Membership < ApplicationRecord
   belongs_to :user
+
+  scope :only_active, -> { where(active: true) }
+  scope :order_by_activity, -> { order(last_active_at: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :memberships
+
+  def find_recent_active_memberships
+    self.memberships.only_active.order_by_activity.limit(5);
+  end
 end
